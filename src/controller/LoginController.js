e66131fadd5e57143e3850d02b831d6eafc54b53
Kin Ishii <kinishii97@gmail.com>
2024-02-17 07:27:07 +0900

    criando validações do login

--- src/controller/LoginController.js
+++ src/controller/LoginController.js
@@ -1,12 +1,8 @@
-const crypto = require('crypto');
 const path = require('path');
+const generateToken = require('../utils/generateToken');
 const readJsonData = require('../services/readJsonData');
 
 const PATH = path.join(__dirname, '..', 'talker.json');
-
-function generateToken() {
-  return crypto.randomBytes(8).toString('hex');
-}
 
 const getTokenFromLogin = async (_req, res) => {
   const token = generateToken();
--- src/routes/LoginRoutes.js
+++ src/routes/LoginRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 
 const router = express.Router();
+const validateLogin = require('../middlewares/login');
 
 const LoginController = require('../controller/LoginController');
 
-router.post('/', LoginController.getTokenFromLogin);
+router.post('/', validateLogin, LoginController.getTokenFromLogin);
 
 module.exports = router;
