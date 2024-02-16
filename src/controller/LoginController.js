97111f47817af0e5f2f8c6aa7fb7729fda7844d2
Kin Ishii <kinishii97@gmail.com>
2024-02-17 08:26:07 +0900

    arrumando erro linter LoginController

--- src/controller/LoginController.js
+++ src/controller/LoginController.js
@@ -1,8 +1,4 @@
-const path = require('path');
 const generateToken = require('../utils/generateToken');
-const readJsonData = require('../services/readJsonData');
-
-const PATH = path.join(__dirname, '..', 'talker.json');
 
 const getTokenFromLogin = async (_req, res) => {
   const token = generateToken();
